a5dfb484a60c60a8d7bd8e80e3c74752488703f6
zamemirs <112723194+zamemirs@users.noreply.github.com>
2023-10-27 17:08:38 +0530

    Update App.test.js
    updated test case

--- src/App.test.js
+++ src/App.test.js
@@ -3,4 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
-  test('renders learn react link',
+  test('renders learn react link', () => {
+    const { getByText } = render(<App />);
+  });
+});
